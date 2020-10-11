793284d325cfdf6495d6eaa38aa4c436813a91e8
Edward Knowles <ed.knowles@stunjelly.com>
2020-10-11 22:17:19 +0100

    fix(app): global css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ['./components/**/*.tsx', './pages/**/*.tsx', './lib/*.tsx'],
+  purge: [
+    './components/**/*.tsx',
+    './hooks/**/*.tsx',
+    './pages/**/*.tsx',
+    './lib/*.tsx',
+  ],
   corePlugins: {
     clear: false,
     divideWidth: false,
